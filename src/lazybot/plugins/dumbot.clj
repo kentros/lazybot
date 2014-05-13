41cc3b01a305660c91db2b55d27ea7754754fb60
Kenny Ingle <kenny.ingle@cerner.com>
2014-05-13 09:20:42 -0500

    adds dumbot plugin to do dumb calls

---
+++ src/lazybot/plugins/dumbot.clj
@@ -0,0 +1,49 @@
+(ns lazybot.plugins.dumbot
+  (:use lazybot.registry)
+  (:require [cheshire.core :as json]
+            [clj-http.client :as http]
+            [clojure.string :as string]))
+
+(use '[clojure.java.shell :only [sh]])
+
+(defplugin
+  (:cmd 
+   "Attempts to make an intelligent reply from arbitrary input." 
+   #{"dumbot"} 
+   (fn [{:keys [args] :as com-m}]
+     (send-message com-m 
+                   (:out (sh "python" "chat.py" (string/join " " args))))))
+
+  (:cmd
+   "Say a random yomomma joke."
+   #{"yomomma"}
+   (fn [{:keys [args] :as com-m}]
+     (send-message com-m
+                   (str (string/join " " args) " " (second (string/split (:body (http/get "http://api.yomomma.info/")) #"joke\":\"|\"}</body>"))))))
+
+  (:cmd
+   "Gives a sales pitch."
+   #{"pitch"}
+   (fn [{:keys [args] :as com-m}]
+     (send-message com-m
+                   (str (:body (http/get "http://itsthisforthat.com/api.php?text")) " With a $10,000 investment, you can be in on the ground floor."))))
+
+  (:cmd
+   "Display a random cat picture."
+   #{"cat"}
+   (fn [{:keys [args] :as com-m}]
+     (send-message com-m
+                   (get (:headers (http/get "http://thecatapi.com/api/images/get" {:follow-redirects false})) "location"))))
+
+  (:cmd
+   "Say a random Chuck Norris joke."
+   #{"chuck"}
+   (fn [{:keys [args] :as com-m}]
+     (send-message com-m
+                   (get-in (json/parse-string (:body (http/get "http://api.icndb.com/jokes/random"))) ["value" "joke"]))))
+  (:cmd
+   "Test."
+   #{"test"}
+   (fn [{:keys [com nick hmask args irc] :as com-m}]
+     (send-message com-m
+                   (.toLowerCase (str hmask))))))
